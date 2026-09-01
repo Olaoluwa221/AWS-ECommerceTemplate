f6f52e25f9641968c768c2799c569b30f0527ddf
Olaoluwa221 <oifeiji@emich.edu>
2026-09-01 00:21:05 -0400

    Setup Health controller and preliminary Products feature module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthController } from './health/health.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProductsModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
